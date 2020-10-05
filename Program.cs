5fda222999c9490ab4f4a64605de010945fafa37
Axel <axel.lefort@hotmail.fr>
2020-10-05 16:48:44 +0200

    Added com with Client

--- Program.cs
+++ Program.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using ServiceStack.Text;
+using SockNet;
+using SockNet.ServerSocket;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ShifuServ
 {
-    class Program
+    class ac
     {
-        static void Main(string[] args)
+        public static void Socketlst()
         {
-            Console.WriteLine("Hello World!");
+            var socketServer = new SocketServer();
+            socketServer.InitializeSocketServer("127.0.0.1", 9999);
+            socketServer.SetReaderBufferBytes(1024);
+            socketServer.StartListening();
+
+            bool openServer = true;
+            while (openServer)
+            {
+                if (socketServer.IsNewData())
+                {
+                    
+                    var data = socketServer.GetData();
+                    // Do whatever you want with data
+                    //var lobby2 = JsonSerializer.DeserializeFromString<LobbyResponse>(socketServer.GetData());
+                    Task.Run(() => DoSomething(data, socketServer));
+                }
+            }
+
+            //.... 
+            socketServer.CloseServer();
+
+
+
+        }
+        private static void DoSomething(KeyValuePair<TcpClient, byte[]> data, SocketServer server)
+        {
+            Console.WriteLine(((IPEndPoint)data.Key.Client.RemoteEndPoint).Address.ToString() + ": " + Encoding.UTF8.GetString(data.Value));
+            server.ResponseToClient(data.Key, "received");
         }
     }
 }
